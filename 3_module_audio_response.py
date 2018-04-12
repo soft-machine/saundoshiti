537610573a28c33e38a2eed37b419b70fab7e635
softmachine <jason.cook@esapyrenees.fr>
2018-04-12 15:50:10 +0000

    finalised codes with 3 RFID modules implemented

---
+++ 3_module_audio_response.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+from subprocess import call
+import RPi.GPIO as GPIO
+import MFRC522
+import signal
+from pydub import AudioSegment
+from pydub.playback import play
+
+continue_reading = True
+
+#this list designates our 3 RFID readers by the RPi pins to which each RST is connected
+rstPins=[15,18,22]
+#make a counter and start at 0
+rstCount = 0;
+countLimit=len(rstPins)
+
+#setup our hardware RST pins and set them low to begin
+GPIO.setmode(GPIO.BOARD)
+for i in range(len(rstPins)):
+    GPIO.setup(rstPins[i], GPIO.OUT)
+    GPIO.output(rstPins[i], 0)
+
+#audio files. change for wherever you place them in the system
+audio1 = AudioSegment.from_file("/home/pi/audio_files/cow.wav")
+audio2 = AudioSegment.from_file("/home/pi/audio_files/cricket.wav")
+
+# Capture SIGINT for cleanup when the script is aborted
+def end_read(signal,frame):
+    global continue_reading
+    print "Ctrl+C captured, ending read."
+    continue_reading = False
+    GPIO.cleanup()
+
+# Hook the SIGINT
+signal.signal(signal.SIGINT, end_read)
+# Create an object of the class MFRC522
+MIFAREReader = MFRC522.MFRC522()
+
+# This loop keeps checking for chips
+while continue_reading:
+    if rstCount < (countLimit-1):
+        rstCount+=1
+    else:
+        rstCount=0
+
+    print "rstCount is ",rstCount
+
+    #activate RFID reader and initiate
+    GPIO.output(rstPins[rstCount], 1)
+    MIFAREReader.MFRC522_Init()
+
+    # Scan for cards    
+    (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+
+    # Get the UID of the card
+    (status,uid) = MIFAREReader.MFRC522_Anticoll()
+
+    # If we have the UID, continue
+    if status == MIFAREReader.MI_OK:
+        if rstCount == 0:
+            call(["espeak","-s140 -ven+18 -z","Module 1"])
+        elif rstCount == 1:
+            call(["espeak","-s140 -ven+18 -z","Module 2"])
+        elif rstCount == 2:
+            call(["espeak","-s140 -ven+18 -z","Module 3"])
+
+        # Print UID
+        cardID = str(uid[3])
+        print "card ID is", cardID
+
+        if int(cardID) == 57:
+            play(audio1)
+        elif int(cardID) == 171:
+            play(audio2)
+
+        # This is the default key for authentication
+        key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
+
+        # Select the scanned tag
+        MIFAREReader.MFRC522_SelectTag(uid)
+
+        # Authenticate
+        status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
+
+        # Check if authenticated
+        if status == MIFAREReader.MI_OK:
+            MIFAREReader.MFRC522_Read(8)
+            MIFAREReader.MFRC522_StopCrypto1()
+        else:
+            print "Authentication error"
+
+    #throw last reader RST pin low 
+    GPIO.output(rstPins[rstCount], 0)
--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # saundoshiti
 student project uses multiple RC522 RFID readers on a single raspberry pi
+
+The codes were built upon the classes originally available here: https://github.com/mxgxw/MFRC522-python
+The file MFRC522.py is only slightly changed to not include the activation of NRSTPD (MFRC522 reset pin)
+By changing this pin's state directly in the code we are able to switch between several different RFID readers.
+The RST pin on all inactive cards must be set LOW. Only one at a time may be set HIGH so as to read it without disturbance from the other connected readers.
+Pydub is being used to read audio files as response to RFID tag recognition.
+The following information is modified from the readme file provided by mxgxw.
+read, write, and dump data from a chip
+
+MFRC522-python
+==============
+A small class to interface with the NFC reader Module MFRC522 on the Raspberry Pi.
+This is a Python port of the example code for the NFC module MF522-AN.
+
+## Requirements
+This code requires you to have SPI-Py installed from the following repository:
+https://github.com/lthiery/SPI-Py
+
+## Examples
+This repository includes two separate codes to manage sound response using 2 separate ID tags and numerous RFID modules. The final code titled "3_module_audio_response.py" is best commented as this was our final goal.
+
+## Pins
+
+All readers are connected to the same following pins
+Only the RST pin is different for each
+
+| Name | Pin # | Pin name   |
+|:------:|:-------:|:------------:|
+| SDA  | 24    | GPIO8      |
+| SCK  | 23    | GPIO11     |
+| MOSI | 19    | GPIO10     |
+| MISO | 21    | GPIO9      |
+| IRQ  | None  | None       |
+| GND  | Any   | Any Ground |
+| 3.3V | 1     | 3V3        |
+
+=== RST pins per reader (A,B,C) ===
+| RST_A| 15    | GPIO22     |
+| RST_B| 18    | GPIO24     |
+| RST_C| 22    | GPIO25     |
+
+## Usage
+Import the class by importing MFRC522 in the top of your script.
+
+## License
+This code and examples are licensed under the GNU Lesser General Public License 3.0.
+
